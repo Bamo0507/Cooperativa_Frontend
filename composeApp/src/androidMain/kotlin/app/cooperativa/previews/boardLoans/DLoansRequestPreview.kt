9bf67e9720ae4d5cd451e4c22d7c548007c0b6a4
adrianap0607 <132533905+adrianap0607@users.noreply.github.com>
2025-05-24 22:05:02 -0600

    Ajustes en las previews de solicitud de prestamo

--- composeApp/src/androidMain/kotlin/app/cooperativa/previews/boardLoans/DLoansRequestPreview.kt
+++ composeApp/src/androidMain/kotlin/app/cooperativa/previews/boardLoans/DLoansRequestPreview.kt
@@ -1,72 +1,101 @@
-//package app.cooperativa.previews.boardLoans
-//
-//import android.content.res.Configuration
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.tooling.preview.Preview
-//import app.cooperativa.data.localdb.SolicitudPrestamoMockData
-//import app.cooperativa.presentation.mainflow.directiva.prestamos.loanRequestDetail.SolicitudPrestamoScreen
-//import app.cooperativa.theme.CoopTheme
-//
-//private val sampleSolicitud = SolicitudPrestamoMockData.getSolicitudById(1)!!
-//
-//@Preview(
-//    name = "Solicitud Detalle Light Mode",
-//    showBackground = true,
-//    uiMode = Configuration.UI_MODE_NIGHT_NO
-//)
-//@Composable
-//fun SolicitudPrestamoDetailPreviewLight() {
-//    CoopTheme {
-//        SolicitudPrestamoScreen(
-//            prestamo = sampleSolicitud,
-//            onBackClick = {}
-//        )
-//    }
-//}
-//
-//@Preview(
-//    name = "Solicitud Detalle Dark Mode",
-//    showBackground = true,
-//    uiMode = Configuration.UI_MODE_NIGHT_YES
-//)
-//@Composable
-//fun SolicitudPrestamoDetailPreviewDark() {
-//    CoopTheme {
-//        SolicitudPrestamoScreen(
-//            prestamo = sampleSolicitud,
-//            onBackClick = {}
-//        )
-//    }
-//}
-//
-//private val sampleSolicitud2 = SolicitudPrestamoMockData.getSolicitudById(2)!!
-//
-//@Preview(
-//    name = "Solicitud Detalle Light Mode",
-//    showBackground = true,
-//    uiMode = Configuration.UI_MODE_NIGHT_NO
-//)
-//@Composable
-//fun SolicitudPrestamoDetailPreviewLight2() {
-//    CoopTheme {
-//        SolicitudPrestamoScreen(
-//            prestamo = sampleSolicitud2,
-//            onBackClick = {}
-//        )
-//    }
-//}
-//
-//@Preview(
-//    name = "Solicitud Detalle Dark Mode",
-//    showBackground = true,
-//    uiMode = Configuration.UI_MODE_NIGHT_YES
-//)
-//@Composable
-//fun SolicitudPrestamoDetailPreviewDark2() {
-//    CoopTheme {
-//        SolicitudPrestamoScreen(
-//            prestamo = sampleSolicitud2,
-//            onBackClick = {}
-//        )
-//    }
-//}
+package app.cooperativa.previews.boardLoans
+
+import android.content.res.Configuration
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.cooperativa.data.localdb.SolicitudPrestamoMockData
+import app.cooperativa.presentation.mainflow.directiva.prestamos.loanRequestDetail.SolicitudPrestamoScreen
+import app.cooperativa.theme.CoopTheme
+
+// Datos de ejemplo para dos solicitudes
+private val sampleSolicitud1 = SolicitudPrestamoMockData.getSolicitudById(1)!!
+private val sampleSolicitud2 = SolicitudPrestamoMockData.getSolicitudById(2)!!
+
+@Preview(
+    name = "Solicitud Detalle Light 1",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Composable
+fun SolicitudPrestamoDetailPreviewLight1() {
+    CoopTheme {
+        SolicitudPrestamoScreen(
+            prestamo = sampleSolicitud1,
+            interestInput = 0f,
+            commentsInput = "",
+            onInterestChange = {},
+            onCommentsChange = {},
+            onApprove = {},
+            onReject = {},
+            onBackClick = {},
+            modifier = Modifier
+        )
+    }
+}
+
+@Preview(
+    name = "Solicitud Detalle Dark 1",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun SolicitudPrestamoDetailPreviewDark1() {
+    CoopTheme {
+        SolicitudPrestamoScreen(
+            prestamo = sampleSolicitud1,
+            interestInput = 0f,
+            commentsInput = "",
+            onInterestChange = {},
+            onCommentsChange = {},
+            onApprove = {},
+            onReject = {},
+            onBackClick = {},
+            modifier = Modifier
+        )
+    }
+}
+
+@Preview(
+    name = "Solicitud Detalle Light 2",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Composable
+fun SolicitudPrestamoDetailPreviewLight2() {
+    CoopTheme {
+        SolicitudPrestamoScreen(
+            prestamo = sampleSolicitud2,
+            interestInput = 0f,
+            commentsInput = "",
+            onInterestChange = {},
+            onCommentsChange = {},
+            onApprove = {},
+            onReject = {},
+            onBackClick = {},
+            modifier = Modifier
+        )
+    }
+}
+
+@Preview(
+    name = "Solicitud Detalle Dark 2",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun SolicitudPrestamoDetailPreviewDark2() {
+    CoopTheme {
+        SolicitudPrestamoScreen(
+            prestamo = sampleSolicitud2,
+            interestInput = 0f,
+            commentsInput = "",
+            onInterestChange = {},
+            onCommentsChange = {},
+            onApprove = {},
+            onReject = {},
+            onBackClick = {},
+            modifier = Modifier
+        )
+    }
+}
