5a68b264b3e91572f1df814145f34692dc151127
adrianap0607 <132533905+adrianap0607@users.noreply.github.com>
2025-08-03 15:00:56 -0600

    Update state variables for fine screen

--- composeApp/src/commonMain/kotlin/app/cooperativa/presentation/mainflow/directiva/manager/fine/DFineManagerState.kt
+++ composeApp/src/commonMain/kotlin/app/cooperativa/presentation/mainflow/directiva/manager/fine/DFineManagerState.kt
@@ -1,10 +1,15 @@
 package app.cooperativa.presentation.mainflow.directiva.manager.fine
 
+import app.cooperativa.domain.directiva.Member
+
 data class DFineManagerState(
+    val memberOptions: List<Member> = emptyList(),
+
     val fineName: String = "",
     val fineAmount: Float = 0.0f,
     val affiliateName: String = "",
-    val affiliateId: String = "",
+    val affiliateId: Int = 0,
+
     val isLoading: Boolean = false,
     val errorMessage: String? = null
 )
