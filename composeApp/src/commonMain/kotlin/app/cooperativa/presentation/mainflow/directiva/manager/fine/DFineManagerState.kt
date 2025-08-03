391cf7c953b45f6831bc0c7a307226a176cb2e14
adrianap0607 <132533905+adrianap0607@users.noreply.github.com>
2025-08-03 12:48:26 -0600

    State variables for fine form

--- composeApp/src/commonMain/kotlin/app/cooperativa/presentation/mainflow/directiva/manager/fine/DFineManagerState.kt
+++ composeApp/src/commonMain/kotlin/app/cooperativa/presentation/mainflow/directiva/manager/fine/DFineManagerState.kt
@@ -1,5 +1,10 @@
 package app.cooperativa.presentation.mainflow.directiva.manager.fine
 
-//data class DFineManagerState(
-//
-//)
+data class DFineManagerState(
+    val fineName: String = "",
+    val fineAmount: Float = 0.0f,
+    val affiliateName: String = "",
+    val affiliateId: String = "",
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
+)
