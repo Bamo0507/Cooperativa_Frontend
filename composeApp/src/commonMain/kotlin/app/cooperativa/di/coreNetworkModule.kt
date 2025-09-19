52149096bda8d25a4339c69512fa9e606d5646ac
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2025-09-19 15:07:53 -0600

    Manejo de multiples endpoints con graphql client

--- composeApp/src/commonMain/kotlin/app/cooperativa/di/coreNetworkModule.kt
+++ composeApp/src/commonMain/kotlin/app/cooperativa/di/coreNetworkModule.kt
@@ -2,12 +2,23 @@ package app.cooperativa.di
 
 import app.cooperativa.core.network.apollo.PaymentApollo
 import com.apollographql.apollo3.ApolloClient
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
+private fun apollo(endpoint: String): ApolloClient =
+    ApolloClient.Builder()
+        .serverUrl(endpoint)
+        .build()
+
 val coreNetworkModule = module {
-    single<ApolloClient> {
-        PaymentApollo(
-            endpoint = "https://dev.cooperativa-isp.cc/graphql/payment"
-        ).client
+    // Cliente para /graphql/payment
+    single(named("payment")) {
+        apollo("https://dev.cooperativa-isp.cc/graphql/payment")
     }
+
+    // Cliente para /graphql/fine
+    single(named("fine")) {
+        apollo("https://dev.cooperativa-isp.cc/graphql/fine")
+    }
+
 }
--- composeApp/src/commonMain/kotlin/app/cooperativa/di/socios/SHistorialViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/app/cooperativa/di/socios/SHistorialViewModelModule.kt
@@ -4,10 +4,20 @@ import app.cooperativa.domain.socios.SHistorialRepository
 import app.cooperativa.domain.socios.SociosHistorialRepository
 import app.cooperativa.presentation.mainflow.socios.historial.mainHistorial.SHistorialViewModel
 import com.apollographql.apollo3.ApolloClient
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val shistorialmodule = module {
+    single<SHistorialRepository> {
+        SociosHistorialRepository(
+            apollo = get<ApolloClient>(named("payment"))
+        )
+    }
+
     factory {
-        SHistorialViewModel(SociosHistorialRepository(get<ApolloClient>()), get())
+        SHistorialViewModel(
+            get(),
+            get()
+        )
     }
 }
