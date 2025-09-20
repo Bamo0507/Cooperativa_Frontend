4f4f779445e660f8cdd25c90bfedf634d647536b
adrianap0607 <132533905+adrianap0607@users.noreply.github.com>
2025-09-19 23:45:19 -0600

    Actualizar VM para recibir prefs para access token y manejar fetch

--- composeApp/src/commonMain/kotlin/app/cooperativa/presentation/mainflow/socios/pagos/pagoStatus/SPagosStatusViewModel.kt
+++ composeApp/src/commonMain/kotlin/app/cooperativa/presentation/mainflow/socios/pagos/pagoStatus/SPagosStatusViewModel.kt
@@ -2,6 +2,7 @@ package app.cooperativa.presentation.mainflow.socios.pagos.pagoStatus
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import app.cooperativa.domain.localstorage.PreferencesLocalStorage
 import app.cooperativa.domain.socios.SPagosStatusRepository
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,31 +12,21 @@ import kotlinx.coroutines.launch
 
 class SPagosStatusViewModel(
     private val repository: SPagosStatusRepository,
-    private val userId: Int = 1
-): ViewModel() {
-    private val _uiState: MutableStateFlow<SPagosStatusState> = MutableStateFlow(
-        SPagosStatusState()
-    )
+    private val prefs: PreferencesLocalStorage
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SPagosStatusState())
     val uiState = _uiState.asStateFlow()
 
-    init {
-        loadData()
-    }
+    init { loadData() }
 
-    fun loadData(){
+    fun loadData() {
         viewModelScope.launch {
-            _uiState.update {
-                it.copy(
-                    isLoading = true,
-                    errorMessage = null
-                )
-            }
-
-            // delay time 1.5s
-            delay(1500)
+            _uiState.update { it.copy(isLoading = true, errorMessage = null) }
 
             try {
-                val pagosUser = repository.getPagoStatusByUser(userId)
+                val accessToken = prefs.getAccessToken()
+                val pagosUser = repository.getPagoStatusByUser(accessToken)
 
                 _uiState.update {
                     it.copy(
@@ -47,11 +38,10 @@ class SPagosStatusViewModel(
                 _uiState.update {
                     it.copy(
                         isLoading = false,
-                        errorMessage = e.message
+                        errorMessage = e.message ?: "Error desconocido"
                     )
                 }
             }
         }
     }
-
 }
