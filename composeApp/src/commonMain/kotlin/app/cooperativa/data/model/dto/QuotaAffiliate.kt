3f74aee5c8cc8a0b2437a4e3396c43b75d153ce7
adrianap0607 <132533905+adrianap0607@users.noreply.github.com>
2025-10-13 23:26:35 -0600

    refactor: Update QuotaAffiliate.kt wrong type on id

--- composeApp/src/commonMain/kotlin/app/cooperativa/data/model/dto/QuotaAffiliate.kt
+++ composeApp/src/commonMain/kotlin/app/cooperativa/data/model/dto/QuotaAffiliate.kt
@@ -1,7 +1,7 @@
 package app.cooperativa.data.model.dto
 
 data class QuotaAffiliate(
-    val idCuota: Int,
+    val idCuota: String,
     val idAsociado: String,
     val identifier: String,
     val montoCuota: Float,
