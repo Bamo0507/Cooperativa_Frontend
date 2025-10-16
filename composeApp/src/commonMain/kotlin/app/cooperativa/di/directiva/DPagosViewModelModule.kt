1ed6941a02821401e862b7c6427e40d40685af11
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2025-10-16 17:14:26 -0600

    feat: add query to show payments

--- composeApp/src/commonMain/kotlin/app/cooperativa/di/directiva/DPagosViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/app/cooperativa/di/directiva/DPagosViewModelModule.kt
@@ -1,12 +1,14 @@
 package app.cooperativa.di.directiva
 
 import app.cooperativa.domain.directiva.DPaymentsRepository
-import app.cooperativa.domain.directiva.MockPaymentsRepository
+import app.cooperativa.domain.directiva.DirectivePaymentsRepository
 import app.cooperativa.presentation.mainflow.directiva.pagos.pagosGeneral.DPaymentsViewModel
+import com.apollographql.apollo3.ApolloClient
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val dpagosmodule = module {
-    single<DPaymentsRepository> { MockPaymentsRepository() }
+    single<DPaymentsRepository> { DirectivePaymentsRepository(get<ApolloClient>(named("payment"))) }
 
-    factory { DPaymentsViewModel( get() ) }
+    factory { DPaymentsViewModel(get()) }
 }
